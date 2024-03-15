33d2aa02f17c091a94b4cde08c04ca57a85b9730
Matis Breillad <mbreillad@gmail.com>
2024-03-15 12:38:04 +0100

    Random Forest avec données 2024

---
+++ Codes/RandomForest2024/rf.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import os
+
+train_bio_ave = pd.read_csv('Data/Data_2024/Environnemental_Data/train/GLC24-PA-train-bioclimatic-average.csv')
+train_bio_mon = pd.read_csv('Data/Data_2024/Environnemental_Data/train/GLC24-PA-train-bioclimatic-monthly.csv')
+train_elev = pd.read_csv('Data/Data_2024/Environnemental_Data/train/GLC24-PA-train-elevation.csv')
+train_human = pd.read_csv('Data/Data_2024/Environnemental_Data/train/GLC24-PA-train-human-footprint.csv')
+train_land = pd.read_csv('Data/Data_2024/Environnemental_Data/train/GLC24-PA-train-landcover.csv')
+train_soilgrid = pd.read_csv('Data/Data_2024/Environnemental_Data/train/GLC24-PA-train-soilgrids.csv')
+
+
+
